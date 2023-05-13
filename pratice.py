cffbfd540d891bfd24e4bf9173755f2734587dcb
jeongmin <wjdals6360@naver.com>
2023-05-13 14:06:05 +0900

    secind commit

--- pratice.py
+++ pratice.py
@@ -5,7 +5,7 @@ print(mystr)
 
 mystr =mystr[:mystr.index(".")]
 print(mystr)
-
+추가됬는지 보자
 password = mystr[:3]+ str(len(mystr)) + str(mystr.count("e"))+ "!"
 
 print("{0}의 비밀번호는 {1} 입니다".format(url , password))
